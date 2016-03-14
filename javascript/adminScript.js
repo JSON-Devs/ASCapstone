c6cef1822def29c23661838d725e3bbe8357afac
jaygags <jasongagnon@Jasons-MacBook-Air.local>
2016-03-14 19:44:59 -0400

    admin page fix

--- javascript/adminScript.js
+++ javascript/adminScript.js
@@ -3,22 +3,17 @@ function loadReports(){
 	loadReported();
 
 }
-function loadNew()
-{
+function loadNew(){
 		$.getJSON( "php/adminPhp.php", { action:"listNew"})
 	.done(function(json){
 		fillNew(json);
 		});
 }
-function fillNew(json)
-{
+function fillNew(json){
 	var count = 0;
 	var str ="<table class = 'adminTable'><tr><th>First Name</th><th>User ID</th><th> Last Name</th><th>Action</th></tr>";
-	for(i=0; i<json.Result.length; i++)
-	{
+	for(i=0; i<json.Result.length; i++){
 	str+= "<tr><td>"+json.Result[i].firstName + "</td><td> " + json.Result[i].userID + "</td><td> " + json.Result[i].lastName +"</td><td><input type='button' class='buttonTest' id='suspendBtn_"+ i +"'onclick ='singleRecord("+json.Result[i].userID+")' value='View' action='#'></td></tr>";
-
-	
 	count = i+1;
 	}
 	
@@ -27,30 +22,24 @@ function fillNew(json)
 	var strrr = "# Of New Profiles To Be Checked: " + count;
 	$('#newUserHeader').html(strrr);
 }
-function singleRecord(ids)
-{
+function singleRecord(ids){
 	 localStorage.setItem("NewUserID", ids );
 	setTimeout(function () {
-					window.location = "adminViewer.html";
-					}, 100);
+			window.location = "adminViewer.html";
+			}, 100);
 }
-function loadReported()
-{
+function loadReported(){
 		$.getJSON( "php/adminPhp.php", { action:"listReported"})
 	.done(function(json){
 		fillReported(json);
 		});
 }
-function fillReported(json)
-{
-		var count = 0;
+function fillReported(json){
+	var count = 0;
 	var str ="<table class = 'adminTable'><tr><th>First Name</th><th>User ID</th><th> Last Name</th><th>Action</th></tr>";
-	for(i=0; i<json.Result.length; i++)
-	{
-	str+= "<tr><td>"+json.Result[i].firstName + "</td><td> " + json.Result[i].userID + "</td><td> " + json.Result[i].lastName +"</td><td><input type='button' class='buttonTest' id='suspendBtn_"+ i +"'onclick ='singleRecord("+json.Result[i].userID+")' value='View' action='#'></td></tr>";
-
-	
-	count = i+1;
+	for(i=0; i<json.Result.length; i++){
+		str+= "<tr><td>"+json.Result[i].firstName + "</td><td> " + json.Result[i].userID + "</td><td> " + json.Result[i].lastName +"</td><td><input type='button' class='buttonTest' id='suspendBtn_"+ i +"'onclick ='singleRecord("+json.Result[i].userID+")' value='View' action='#'></td></tr>";
+		count = i+1;
 	}
 	
 	str+="</table>";
@@ -58,19 +47,20 @@ function fillReported(json)
 	var strrr = "# Of Reported Profiles: " + count;
 	$('#reportedUserHeader').html(strrr);
 }
-function resetLocalStorage()
-{
+function resetLocalStorage(){
 	 localStorage.setItem("userID", "" );
 }
-function pageSecurity(json)
-{
+function pageSecurity(json){
 	var ls = localStorage.getItem("userID");
-		if(json.Result[0].isAdmin == true )
-	{
-		loadReports();
+	if(ls != ""){
+		if(json.Result[0].isAdmin == true){
+			loadReports();
+		}
+		else{
+			window.location = "index.html";
+		}
 	}
-	else
-	{
+	else{
 		window.location = "index.html";
 	}
 }
