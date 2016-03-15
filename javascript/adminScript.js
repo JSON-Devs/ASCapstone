7dfe0cb39e6b217dae6baa1f8a084e17cbab17a6
jaygags <jasongagnon@Jasons-MacBook-Air.local>
2016-03-14 20:14:54 -0400

    fixed format and documented admin js

--- javascript/adminScript.js
+++ javascript/adminScript.js
@@ -1,55 +1,67 @@
+//Load New and Reported
 function loadReports(){
 	loadNew();
 	loadReported();
-
 }
+
+//Load New Profiles
 function loadNew(){
-		$.getJSON( "php/adminPhp.php", { action:"listNew"})
+	$.getJSON( "php/adminPhp.php", { action:"listNew"})
 	.done(function(json){
 		fillNew(json);
-		});
+	});
 }
+
+//Fill New Profiles
 function fillNew(json){
 	var count = 0;
-	var str ="<table class = 'adminTable'><tr><th>First Name</th><th>User ID</th><th> Last Name</th><th>Action</th></tr>";
+	var str = "<table class = 'adminTable'><tr><th>First Name</th><th>User ID</th><th> Last Name</th><th>Action</th></tr>";
 	for(i=0; i<json.Result.length; i++){
-	str+= "<tr><td>"+json.Result[i].firstName + "</td><td> " + json.Result[i].userID + "</td><td> " + json.Result[i].lastName +"</td><td><input type='button' class='buttonTest' id='suspendBtn_"+ i +"'onclick ='singleRecord("+json.Result[i].userID+")' value='View' action='#'></td></tr>";
-	count = i+1;
+		str += "<tr><td>"+json.Result[i].firstName + "</td><td> " + json.Result[i].userID + "</td><td> " + json.Result[i].lastName +"</td><td><input type='button' class='buttonTest' id='suspendBtn_"+ i +"'onclick ='singleRecord("+json.Result[i].userID+")' value='View' action='#'></td></tr>";
+		count = i + 1;
 	}
-	
-	str+="</table>";
+	str += "</table>";
 	$('#reviewDiv').html(str);
 	var strrr = "# Of New Profiles To Be Checked: " + count;
 	$('#newUserHeader').html(strrr);
 }
+
+//Load Correct Profile To Review
 function singleRecord(ids){
-	 localStorage.setItem("NewUserID", ids );
+	localStorage.setItem("NewUserID", ids );
 	setTimeout(function () {
-			window.location = "adminViewer.html";
-			}, 100);
+		window.location = "adminViewer.html";
+	}, 100);
 }
+
+//Load Reported Profiles
 function loadReported(){
-		$.getJSON( "php/adminPhp.php", { action:"listReported"})
+	$.getJSON( "php/adminPhp.php", { action:"listReported"})
 	.done(function(json){
 		fillReported(json);
-		});
+	});
 }
+
+//Fill Reported Profiles
 function fillReported(json){
 	var count = 0;
-	var str ="<table class = 'adminTable'><tr><th>First Name</th><th>User ID</th><th> Last Name</th><th>Action</th></tr>";
+	var str = "<table class = 'adminTable'><tr><th>First Name</th><th>User ID</th><th> Last Name</th><th>Action</th></tr>";
 	for(i=0; i<json.Result.length; i++){
-		str+= "<tr><td>"+json.Result[i].firstName + "</td><td> " + json.Result[i].userID + "</td><td> " + json.Result[i].lastName +"</td><td><input type='button' class='buttonTest' id='suspendBtn_"+ i +"'onclick ='singleRecord("+json.Result[i].userID+")' value='View' action='#'></td></tr>";
-		count = i+1;
+		str += "<tr><td>"+json.Result[i].firstName + "</td><td> " + json.Result[i].userID + "</td><td> " + json.Result[i].lastName +"</td><td><input type='button' class='buttonTest' id='suspendBtn_"+ i +"'onclick ='singleRecord("+json.Result[i].userID+")' value='View' action='#'></td></tr>";
+		count = i + 1;
 	}
-	
-	str+="</table>";
+	str += "</table>";
 	$('#reportedDiv').html(str);
 	var strrr = "# Of Reported Profiles: " + count;
 	$('#reportedUserHeader').html(strrr);
 }
+
+//Reset Local Storage UserID
 function resetLocalStorage(){
 	 localStorage.setItem("userID", "" );
 }
+
+//Page Security
 function pageSecurity(json){
 	var ls = localStorage.getItem("userID");
 	if(ls != ""){
@@ -64,22 +76,23 @@ function pageSecurity(json){
 		window.location = "index.html";
 	}
 }
+
+//Document Ready
 $(document).ready(function(){
 	$("#logout").click(function(){
 		resetLocalStorage();
 		setTimeout(function () {
-					window.location = "index.html";
-					}, 100);
+			window.location = "index.html";
+		}, 100);
 	});		
 }); 
+
+//Document Load
 $(window).load(function(){
 	var ls = localStorage.getItem("userID");
- console.log(ls);
-		$.getJSON( "php/adminPhp.php", { action:"checkAdmin", userID: ls})
+	$.getJSON( "php/adminPhp.php", { action:"checkAdmin", userID: ls})
 	.done(function(json){
 		pageSecurity(json);
-		});
-	
-	
+	});
 });
 
--- javascript/adminViewer.js
+++ javascript/adminViewer.js
@@ -1,19 +1,19 @@
-//fill user
-function loadUser()
-{
+//Load User Info
+function loadUser(){
 	var ls = localStorage.getItem("NewUserID");
-		$.getJSON( "php/php_queries.php", { action:"fillUser", userID: ls})
+	$.getJSON( "php/php_queries.php", { action:"fillUser", userID: ls})
 	.done(function(json){
 		fillUser(json);
-		});
+	});
 }
-function fillUser(json)
-{
+
+//Fill User Info
+function fillUser(json){
 	var str = "";
-	str+= "<table><tr><td>"+json.Result[0].firstName + "</td><td> " + json.Result[0].userID + "</td><td> " + json.Result[0].lastName +"</td></tr>";
+	str += "<table><tr><td>"+json.Result[0].firstName + "</td><td> " + json.Result[0].userID + "</td><td> " + json.Result[0].lastName +"</td></tr>";
 	str += "<tr><td>"+json.Result[0].city + "</td><td> " + json.Result[0].state + "</td><td> " + json.Result[0].zip + "</td><td> " + json.Result[0].phoneNumber + "</td></tr>";
-	str+= "<tr><td> Admin:" + json.Result[0].isAdmin + "</td><td> Active: " + json.Result[0].isActive + "</td><td> Reported:" + json.Result[0].isReported + "</td><td> New: " + json.Result[0].isNew +"</td></tr>";
-	str+="</table>";
+	str += "<tr><td> Admin:" + json.Result[0].isAdmin + "</td><td> Active: " + json.Result[0].isActive + "</td><td> Reported:" + json.Result[0].isReported + "</td><td> New: " + json.Result[0].isNew +"</td></tr>";
+	str += "</table>";
 	$('#User').html(str);
 	var reported = json.Result[0].isReported;
 	if(reported == true)
@@ -28,160 +28,178 @@ function fillUser(json)
 	}
 	$('#buttons').html(strr);
 }
-//fill cover
+
+//Load Cover Letter
 function loadCover(){
 	var ls = localStorage.getItem("NewUserID");
-	
 	$.getJSON( "php/php_queries.php", { action:"fillCover", userID: ls } )
 	.done(function(json){
 		fillCover(json);
-		});
+	});
 }
+
+//Fill Cover Letter
 function fillCover(json){
 	var coverLett = "<h4> Cover Letter </h4>";
-		 coverLett += json.Result[0].coverLetter;
-		if(coverLett.length !=0 || coverLett.length != null )
-		{
-			$('#Cover').html(coverLett);
-		}
+	coverLett += json.Result[0].coverLetter;
+	if(coverLett.length !=0 || coverLett.length != null )
+	{
+		$('#Cover').html(coverLett);
 	}
-//fill Video
+}
+	
+//Load Video
 function loadVideo(){
 	var ls = localStorage.getItem("NewUserID");
-	
 	$.getJSON( "php/php_queries.php", { action:"fillVideo", userID: ls } )
 	.done(function(json){
-		
 		fillVideo(json);
-		
-		});
-	
-	
+	});
 }
-function fillVideo(json){
 
-	
+//Fill Video
+function fillVideo(json){
 	if(json.Result == false)
 	{
-	console.log(json.Result);
+		console.log(json.Result);
 	}
 	else
 	{
 		var videoLin = 'https://youtu.be/' + json.Result.videoLink;
-	$("#txtLink").val(videoLin);
-	var vidFrame = '<iframe width="450" height="253" src="https://www.youtube.com/embed/' + json.Result.videoLink + '?rel=0" frameborder="0" allowfullscreen></iframe>'
-	$("#Video").html(vidFrame);
+		$("#txtLink").val(videoLin);
+		var vidFrame = '<iframe width="450" height="253" src="https://www.youtube.com/embed/' + json.Result.videoLink + '?rel=0" frameborder="0" allowfullscreen></iframe>'
+		$("#Video").html(vidFrame);
 	}
+}
 
-}
-function accept()
-{
+//PHP Action Buttons
+
+//Accept New Profile
+function accept(){
 	var ls = localStorage.getItem("NewUserID");
-				$.getJSON( "php/php_queries.php", { action:"acceptUser", userID: ls})
+	$.getJSON( "php/php_queries.php", { action:"acceptUser", userID: ls})
 }
-function decline()
-{
+
+//Decline New Profile
+function decline(){
 	var ls = localStorage.getItem("NewUserID");
-				$.getJSON( "php/php_queries.php", { action:"declineUser", userID: ls})
+	$.getJSON( "php/php_queries.php", { action:"declineUser", userID: ls})
 }
-function suspend()
-{
+
+//Suspend Reported Profile
+function suspend(){
 	var ls = localStorage.getItem("NewUserID");
-				$.getJSON( "php/php_queries.php", { action:"suspendUser", userID: ls})
+	$.getJSON( "php/php_queries.php", { action:"suspendUser", userID: ls})
 }
-function declineReport()
-{
+
+//Decline Report
+function declineReport(){
 	var ls = localStorage.getItem("NewUserID");
-				$.getJSON( "php/php_queries.php", { action:"declineReport", userID: ls})
+	$.getJSON( "php/php_queries.php", { action:"declineReport", userID: ls})
 }
-function btnAccept()
-{
-		accept();
-			setTimeout(function () {
-					window.location = "adminPage.html";
-					}, 100)	
-				
-	}
-function btnDecline()
-{
-				decline();
-				setTimeout(function () {
-					window.location = "adminPage.html";
-					}, 100)	
-	
+
+//Location Action Buttons
+
+//Accept New Profile
+function btnAccept(){
+	accept();
+	setTimeout(function () {
+		window.location = "adminPage.html";
+	}, 100)			
 }
-function btnSuspend()
-{
+
+//Decline New Profile
+function btnDecline(){
+	decline();
+	setTimeout(function () {
+		window.location = "adminPage.html";
+	}, 100)	
+}
+
+//Suspend Reported Profile
+function btnSuspend(){
 	suspend();
-				setTimeout(function () {
-					window.location = "adminPage.html";
-					}, 100)	
+	setTimeout(function () {
+		window.location = "adminPage.html";
+	}, 100)	
 }
-function btndeclineReport()
-{
+
+//Decline Report
+function btndeclineReport(){
 	declineReport();
-				setTimeout(function () {
-					window.location = "adminPage.html";
-					}, 100)	
+	setTimeout(function () {
+		window.location = "adminPage.html";
+	}, 100)	
 }
-function loadEducation()
-{
-var ls = localStorage.getItem("NewUserID");
-	//ls = 113;
+
+//Load Education
+function loadEducation(){
+	var ls = localStorage.getItem("NewUserID");
 	$.getJSON( "php/php_queries.php", { action:"listEducation", userID: ls})
 	.done(function(json){
 		listSchoolsSecondStage(json);
-		});
-	
+	});
 }
-function listSchoolsSecondStage(json)
-{
-	
-		var str = "<tr><th>Education History</th></tr>";
-	
-		for (i=0; i<json.Result.length; i++) {
-			str += "<tr>";
-			str += "<td>" + json.Result[i].school + "</td>";
-			str += "<td>" + json.Result[i].degree + "</td>";
-			
-			
-			str += "</tr>";
-			console.log("k");
-			
-		}
-	
-		$('#Education').html(str);
+
+//Fill Education Info
+function listSchoolsSecondStage(json){
+	var str = "<tr><th>Education History</th></tr>";
+	for (i=0; i<json.Result.length; i++) {
+		str += "<tr>";
+		str += "<td>" + json.Result[i].school + "</td>";
+		str += "<td>" + json.Result[i].degree + "</td>";	
+		str += "</tr>";
+	}
+	$('#Education').html(str);
 }
-function loadEmployer()
-{
+
+//Load Employer
+function loadEmployer(){
 	var ls = localStorage.getItem("NewUserID");
 	
 	$.getJSON( "php/php_queries.php", { action:"listEmployer", userID: ls})
 	.done(function(json){
 		listEmployersSecondStage(json);
-		});
+	});
 }
-function listEmployersSecondStage(json)
-{
-		var str = "<tr><th>Employment History</th></tr>";
-		for (i=0; i<json.Result.length; i++) {
-			str += "<tr>";
-			str += "<td>" + json.Result[i].employerName + "</td>";
-			str += "<td>" + json.Result[i].position + "</td>";
-			
-			str += "</tr>";
-		
+
+//Fill Employer Info
+function listEmployersSecondStage(json){
+	var str = "<tr><th>Employment History</th></tr>";
+	for (i=0; i<json.Result.length; i++) {
+		str += "<tr>";
+		str += "<td>" + json.Result[i].employerName + "</td>";
+		str += "<td>" + json.Result[i].position + "</td>";
+		str += "</tr>";
+	}
+	$('#Employer').html(str);
+}
+
+//Page Security
+function pageSecurity(json){
+	var ls = localStorage.getItem("userID");
+	if(ls != ""){
+		if(json.Result[0].isAdmin == true){
+			loadUser();
+			loadCover();
+			loadVideo();
+			loadEducation();
+			loadEmployer();
 		}
-		$('#Employer').html(str);
+		else{
+			window.location = "index.html";
+		}
+	}
+	else{
+		window.location = "index.html";
+	}
 }
+
+//Document Load
 $(window).load(function(){
-	loadUser();
-	loadCover();
-	loadVideo();
-	loadEducation();
-	loadEmployer();
-	
-	
-	
-	
+	var ls = localStorage.getItem("userID");
+	$.getJSON( "php/adminPhp.php", { action:"checkAdmin", userID: ls})
+	.done(function(json){
+		pageSecurity(json);
+	});
 });
